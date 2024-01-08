ddd5456d713311ae26f5e2ba0e74a9f87d53b1ff
Pascal Libaud <pascal.libaud@viseo.com>
2024-01-08 17:45:59 +0100

    Correction de la date

--- src/frontend/src/shared/datetime.ts
+++ src/frontend/src/shared/datetime.ts
@@ -11,7 +11,7 @@ export const setCurrentDate = (c: CurrentDate): void => {
 export const isoDateTime = (d: Date): string => { return d.toISOString() }
 
 export const formatDate = (d: Date): string => {
-    const day = ('0' + d.getDay()).slice(-2)
+    const day = ('0' + d.getDate()).slice(-2)
     const month = ('0' + d.getMonth() + 1).slice(-2)
     const year = d.getFullYear()
 
