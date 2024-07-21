e84aaeb3070334cf5122ce8da12d2233c638cf15
Pascal Libaud <pascal.libaud@viseo.com>
2024-07-21 22:17:26 +0200

    little fix

--- src/frontend/src/shared/datetime.ts
+++ src/frontend/src/shared/datetime.ts
@@ -12,7 +12,7 @@ export const isoDateTime = (d: Date): string => { return d.toISOString() }
 
 export const formatDate = (d: Date): string => {
     const day = ('0' + d.getDate()).slice(-2)
-    const month = ('0' + d.getMonth() + 1).slice(-2)
+    const month = ('0' + (d.getMonth() + 1)).slice(-2)
     const year = d.getFullYear()
 
     return `${day}/${month}/${year}`
